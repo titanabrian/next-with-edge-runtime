ede642de8b28a8441bef9a6d101cc73da600fd0e
Titan Abriansyah Indihatmoko <titanabrian.05@gmail.com>
2024-03-17 12:03:29 +0700

    fix: add key to log item

--- components/log.tsx
+++ components/log.tsx
@@ -11,9 +11,9 @@ const LogEvent = ({logs}: LogEventInterface) => {
   return (
     <div className="mx-auto rounded-lg m-12 max-w-3xl shadow-sm p-5">
       <h1 className="text-2xl font-bold text-gray-900 sm:text-3xl"><span className="mr-5">Response Logging</span><Spinner/></h1>
-      {logs.map((item: any) => (
+      {logs.map((item: any, key: any) => (
         <div className="mt-10">
-          <LogItem title={item.title} log={item.log} isError={item.isError}/>
+          <LogItem key={key} title={item.title} log={item.log} isError={item.isError}/>
         </div>
       ))}
     </div>
